70cb3590a185564e3a573ce566765112a708a5b1
Ford Cullen <fcullen@usc.edu>
2024-04-21 06:56:34 +0000

    Completed HW5

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,15 +21,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
-
+bool isValid(const AvailabilityMatrix& avail, size_t day, Worker_T worker);
+bool backtrackSch(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, DailySchedule& curr, std::vector<size_t> numShiftsWorked,size_t day);
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
     const size_t maxShifts,
-    DailySchedule& sched
+    DailySchedule& sched //end result. finalized with true/ return statement
 )
 {
     if(avail.size() == 0U){
@@ -37,9 +37,45 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
-
-
-
-
+		size_t numWorkers = avail[0].size();
+		size_t numDays = avail.size();
+		std::vector<size_t> numShiftsWorked(numWorkers,0);
+		DailySchedule curr(numDays);
+		//cout << curr.size();
+		bool result = backtrackSch(avail, dailyNeed, maxShifts, sched, curr, numShiftsWorked, 0);
+		cout << curr[0].size();
+		return result;
 }
 
+bool isValid(const AvailabilityMatrix& avail, size_t day, Worker_T worker) { //check if a day is valid for a worker to work (doesnt include num of shifts worked)
+    return avail[day][worker];
+}
+
+bool backtrackSch(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, DailySchedule& curr, std::vector<size_t> numShiftsWorked,size_t day)
+{
+	size_t numWorkers = avail[0].size();
+	size_t numDays = avail.size();
+	//if at end of work week 
+	if (day == numDays){
+		sched = curr;
+		return true;
+	}
+	size_t workersSched = 0;
+	for (Worker_T w = 0; w < numWorkers; w++)
+	{
+		if (isValid(avail, day, w) && numShiftsWorked[w] < maxShifts)//if work day is valid and worker is not over booked attempt to add them to schedule
+		{
+			curr[day].push_back(w);
+			numShiftsWorked[w]++;
+			workersSched += 1;
+			if (workersSched < dailyNeed)
+				continue;
+			if (backtrackSch(avail, dailyNeed, maxShifts, sched, curr, numShiftsWorked, day+1)) ///backtrace worker
+				return true;
+		}	
+		if (!curr[day].empty()) 
+			curr[day].pop_back();//if fails then they are removed and others are tried
+		numShiftsWorked[w] = numShiftsWorked[w] - 1;
+	}
+	return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -1,4 +1,4 @@
-#ifndef RECCHECK
+	#ifndef RECCHECK
 // For debugging
 #include <iostream>
 // For std::remove
@@ -13,7 +13,9 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+bool contains(std::string&guess, const std::string in);
+std::string wordleHelp(int n, std::string& guess, const std::string& in, 
+const std::string& floating, const std::set<std::string>& dict, std::set<std::string>& returnSet);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +24,82 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+		int n = in.length();
+		std::set<std::string> returnSet;
+		//std::string hi = "nd";
+		//std::string g = "dint";
+		//bool jim =  contains( g, hi );
+		//cout << "----------" <<jim << endl;
+		std::string guess = in;
+		wordleHelp(n, guess, in, floating, dict, returnSet);
+		returnSet.erase("");
+		return returnSet;
 }
 
 // Define any helper functions here
+// returns if the string contains the letters in guess
+bool contains(std::string&guess, const std::string in)
+{
+	//cout << guess <<" " << in;
+	for (int i = 0; i < in.length(); i++)
+	{
+		//cout << i << in[i] << endl;
+		if (guess.find(in[i])==std::string::npos) //check if char is in word at atleast one instance
+		{
+			//cout << endl;
+			return false;
+		}
+	}
+	//cout << ":)" << endl;
+	return true;
+}
+// returns a string that can be found in dict (using recursion)
+std::string wordleHelp(int n, std::string& guess, const std::string& in, 
+const std::string& floating, const std::set<std::string>& dict,  std::set<std::string>& returnSet)
+// a = 97 z = 122
+{
+	//cout << "called function" << endl;
+	string alph = "abcdefghijklmnopqrstuvwxyz";
+	if (n <= 0)
+	{
+		if (dict.find(guess) != dict.end() && contains(guess, floating) == true)
+		{
+			//cout << guess << endl;
+			returnSet.insert(guess);
+			return guess;
+		}
+		return "";
+	}
+	else
+	{
+		if (in[n-1] == '-') //if the space is blank
+		{
+			 //use floating 
+			 if (floating != "")
+			 {
+					for (int i = 0; i < floating.length(); i++) //itterates through the floating
+					{
+						guess[n-1] = floating[i];
+						
+						wordleHelp(n-1, guess, in, floating, dict, returnSet);  //floating unchanged (case: double letters)
+						wordleHelp(n-1,guess, in, floating, dict, returnSet); //floating remove c 
+					}
+			 }
+			 //skip floating used in other position case
+			 for (char c: alph)
+			 {
+					guess[n-1] = c;
+					wordleHelp(n-1,guess, in, floating, dict, returnSet);
+					
+			 }
+		}
+		else //space is filled
+		{
+			//cout << in[n-1] << guess[n-1] << endl;
+			n = n-1;
+			wordleHelp(n, guess, in, floating, dict, returnSet);
+			//update n and try again
+		}
+	}
+	return "";
+}
